e4a2377d172efb92e657cb77613823600d1b30ef
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-12-15 23:30:53 -0300

    config body-parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cannonRouter = require('./cannon/cannon.route');
+const bodyParser = require('body-parser')
 const app = express();
+
+// Config 
+    app.use( bodyParser.urlencoded({ extended: true }) )
+    app.use( bodyParser.json() )
+
 
 app.get('/', (req, res) => {
     res.status(200).send({
@@ -8,6 +14,7 @@ app.get('/', (req, res) => {
         status: true
     })
 })
+
 
 app.use('/cannon', cannonRouter)
 module.exports = app;
