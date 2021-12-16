2575183e150b342f83db8f8ae79595222496a9f7
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-12-16 20:57:17 -0300

    fix findBydId and save

--- src/cannon/cannon.service.js
+++ src/cannon/cannon.service.js
@@ -1,18 +1,43 @@
+const HTTP = require('../utils/statusHTTP');
 const Cannon = require('./models/cannon.model')
+
 
 class CannonService { 
     
     async findById(id) {
-        return Cannon.findById(id, (err) => { 
-            if(err) return err;
-         });
+        try {
+            return  { ...HTTP.ok(), object: await Cannon.findById(id) }
+        }
+        catch(e) {
+            return HTTP.internalServer()            
+        }
+    } 
+
+    async findAll() {
+        try {
+            return { ...HTTP.ok(), objects: await Cannon.find() }
+        }
+        catch(e) { 
+            console.log(e)
+            return HTTP.internalServer()
+        }       
     }
 
     async save(data) {
-        const cannon = new Cannon(data)
-        return cannon.save((err) => {
-            console.log(err)
-        });
+        
+        const cannon = new Cannon(data);
+        const error = cannon.validateSync()
+
+        if (error) 
+            return HTTP.badRequest(error.message)
+
+        try {
+            cannon.save();
+            return HTTP.created('Cannon created');
+        }
+        catch (e) {
+            return HTTP.internalServer();
+        }
     }
 
     async update(cannon) {
