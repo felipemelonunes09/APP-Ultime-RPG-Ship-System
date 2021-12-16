e57a8172f292926e52e3f066264cdf1f73f66c37
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-12-16 20:56:45 -0300

    refactoring save findBydId findAll response methods

--- src/cannon/cannon.controller.js
+++ src/cannon/cannon.controller.js
@@ -4,12 +4,21 @@ const cannonService = require('./cannon.service')
 class CannonController {
 
     async save(req, res) {
-        await cannonService.save(req.body);
-        res.send('Ok')
+        const result = await cannonService.save(req.body);
+        res.status(result.code).send(result)
     }
 
     async findById(req, res) {
-        await cannonService.findById(id);
+        
+        const id = req.params.id
+        const result = await cannonService.findById(id);
+        
+        res.status(result.code).send(result)
+    }
+
+    async findAll(req, res) {
+        const result = await cannonService.findAll();
+        res.status(result.code).send(result)
     }
 
     async delete(req, res) {
