6a131f93f35c437d79df0f4b6bedac72e75a6f6e
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-12-22 19:53:59 -0300

    fix dotenv path

--- bin/main.js
+++ bin/main.js
@@ -3,8 +3,9 @@
 const app = require('../src/app');
 const mongoose = require('mongoose');
 
-require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '../.env' });
 
+// warning
 const PORT =            process.env.PORT    || 8081;
 const DB_NAME =         process.env.DB_NAME || 'redirectDb';
 const DB_PORT =         process.env.DB_PORT || 27017;
