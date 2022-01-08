2c5f11a50ea916ed1815f80ae3393f481b94bb15
felipemelonunes09 <felipe.melonunes09@gmail.com>
2022-01-07 23:06:52 -0300

    _

--- src/app/adm/adm.module.ts
+++ src/app/adm/adm.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { EmptyListBlankslateComponent } from "../static/empty-list-blankslate/empty-list-blankslate.component";
 import { AdmComponent } from "./adm.component";
 import { AdmRoutingModule } from "./adm.routing.module";
@@ -10,6 +11,7 @@ import { ShipsComponent } from "./ships/ships.component";
 
 @NgModule({
     imports: [
+        BrowserModule,
         CommonModule,
         AdmRoutingModule,
         HttpClientModule
--- src/app/adm/parts/parts.component.ts
+++ src/app/adm/parts/parts.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartsComponent implements OnInit {
 
+  public parts: any = []
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isPartsEmpty() : Boolean {
+    return this.parts.length == 0
   }
 
 }
