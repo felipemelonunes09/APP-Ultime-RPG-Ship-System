66493bad2def9502f0ff4bd8296fb73e96835cfe
HenryMiyawaki <miyawakihenry@gmail.com>
2022-01-13 01:07:19 -0300

    fix variable name errors

--- src/app/adm/ships/ships.service.ts
+++ src/app/adm/ships/ships.service.ts
@@ -2,13 +2,12 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ServiceModel } from "src/utils/service-model";
 
-
 @Injectable({ 
     providedIn: 'root'
 })
-export class PartsService extends ServiceModel {
+export class ShipService extends ServiceModel {
     constructor(private partHttp: HttpClient) {
         super(partHttp);
-        this.routeName = 'ship-part';
+        this.routeName = 'ship';
     }
 }
