a6585e74ad562681d82a0a3b9b1c1ab77fc617ee
felipemelonunes09 <felipe.melonunes09@gmail.com>
2022-01-19 21:21:40 -0300

    _

---
+++ src/app/adm/parts/create-parts/create-parts.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreatePartsComponent } from './create-parts.component';
+
+describe('CreatePartsComponent', () => {
+  let component: CreatePartsComponent;
+  let fixture: ComponentFixture<CreatePartsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreatePartsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreatePartsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
