f13b71716413f9b12640589e8d4702c56ff32766
felipemelonunes09 <felipe.melonunes09@gmail.com>
2022-01-20 23:19:21 -0300

    _

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const cannonRouter = require('./cannon/cannon.route');
 const bodyParser = require('body-parser');
 const shipPartsRouter = require('./ship-parts/ship-parts.route');
 const shipRouter = require('./ship/ship.route');
+const cors = require('cors')
+
 const app = express();
+app.use(cors())
+
 
 // Config 
     app.use( bodyParser.urlencoded({ extended: true }) )
