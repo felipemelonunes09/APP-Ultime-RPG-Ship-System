4214b20fa4f5f8a677d7c6fafe9820691221ac3d
felipemelonunes09 <felipe.melonunes09@gmail.com>
2022-01-20 22:46:20 -0300

    more implementatition of sweet alert and response from the server

--- src/app/adm/parts/create-parts/create-parts.component.ts
+++ src/app/adm/parts/create-parts/create-parts.component.ts
@@ -21,17 +21,42 @@ export class CreatePartsComponent implements OnInit {
   create(values: any) : any {
 
     this.loading = true
+    console.log(values)
     this.partsService.create( JSON.parse(values) ).subscribe(
       res => { console.log(res) },
       err => { 
         this.loading = false
-        Swal.fire({
-          title: 'Alo servidor ? cade você ?',
-          html: 'Desculpa mas infelizmente o servidor me deixou no vácuo, verifique o seu meio de comunicação com a internet',
-          icon: 'question',
-          timer: 3*1000,
-          background: '#0d1117'
-        })
+        console.log(err)
+        if (err.status == 0) { 
+          
+          Swal.fire({
+            title: 'Hello server ? where are you  ?',
+            html: 'sorry but a connection with the server could not be completed',
+            icon: 'question',
+            timer: 4*1000,
+            background: '#0d1117'
+          })
+        }
+        else {
+          if (err.status == 400) {
+            console.log(err.error.message)
+              Swal.fire({
+                title: 'Bad Request',
+                html: `Cause: ${err.error.message}`,
+                icon: 'error',
+                background: '#0d1117'
+              })
+          }
+          else {
+            Swal.fire({
+              title: 'Fatal error',
+              html: 'We could not find the error origin, please try again later',
+              icon: 'error',
+              timer: 4*1000,
+              background: '#0d1117'
+            })
+          }
+        }
       }
     )
   }
